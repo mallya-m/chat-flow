fb1d58fa4c7719998694827d80a46b54353083be
Mallya <mallyamoni035@gmail.com>
2026-04-07 23:04:54 +0530

    mongodb connected, user model created

--- server/index.js
+++ server/index.js
@@ -1,18 +1,49 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
-const port = 5000 ;
+const port = process.env.PORT || 5000 ;
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) =>{
+        console.log('MongoDB connection failed:', error)
+    })
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send("Chat flow server is running ");
 })
-app.get("/api/hello",(req,res)=>{
-    res.json({
-        message:"welcome to Chatflow API",
-        version:"1.0",
-        status:"active"
-    })
+app.post("/api/test-user",async(req,res)=>{
+    try{
+        const User = require('./models/User');
+
+        const newUser = new User({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        })
+
+        const savedUser = await newUser.save()
+
+        res.status(201).json({
+            message: "User saved successfully",
+            user : savedUser
+        })
+    }catch(error){
+        res.status(400).json({
+            message: "Error saving user",
+            error : error.message
+        })
+
+    }
 });
 
 app.listen(port,()=>{
